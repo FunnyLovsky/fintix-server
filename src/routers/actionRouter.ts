c6f580541838ef4d652f8fc5b8c09cd097a0f3f7
Andrey Veselovsky <142045012+FunnyLovsky@users.noreply.github.com>
2024-01-15 21:19:58 +0300

    fix: action router

--- src/routers/actionRouter.ts
+++ src/routers/actionRouter.ts
@@ -4,9 +4,9 @@ import { ActionController } from '../controllers/actionsController';
 const router = Router();
 
 router.get('/actions', ActionController.getActions);
-router.post('/create', ActionController.create);
-router.put('/update', ActionController.update);
-router.delete('/delete', ActionController.delete);
+router.post('/actions', ActionController.create);
+router.put('/actions', ActionController.update);
+router.delete('/actions', ActionController.delete);
 router.post('/import', ActionController.parseFile);
 router.post('/export', ActionController.createFile);
 
